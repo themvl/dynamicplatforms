1b34fd734f7534c64064570eac930ad75206963b
mvl <marnicq_the_best@hotmail.com>
2019-06-25 00:43:12 +0200

    Added significant comments and code structure.

--- dynamicplatform.gd
+++ dynamicplatform.gd
@@ -5,66 +5,62 @@ const style_script = preload("dynamicplatformstyle.gd")
 export(Resource) var style = null
 onready var path = $Path2D
 
+#determines wheter the curve is closed or not when it is it draws a filltexture and sets the end and start points to the same position
 export var closed:bool = true
-export var thicknes:float = 10
+#determines the thickness of the border
+export var thicknes:float = 30
+#this draws red dots or red vertex at points of bad polygons that normally have been fixed
 export var mark_bad:bool = true
 
+#determines what method of fixing bad polygons is used 
+#quad switch is the older method and just switches the 2 points
+#the other method uses normals to set the points at a median point offset by their normals by a small margin
 export var quad_switch:bool = false
+
+#determines edge draw method the older method draws full textures and is way less heavy to draw but much worse and not supported
+#the new method segments the texture based on the bake interval of the curve
 export var segmented:bool = true
+#determines if corners are drawn this is not always desired
 export var drawcorners:bool = true
+#determines between which angles a corner will be drawn
+export var angle_treshold = Vector2(30,120)
 
 class_name dynamicplatform
-
-func _init():
-	pass
-
-
-func _ready():
-	print("ready")
-
 
 func _enter_tree():
 	if style == null:
 		style = Resource.new()
 		style.set_script(style_script)
 
+#function that draws a point
 func drawPoint(var pos, var color):
 	draw_circle(pos, 1, color)
 	
 func resetpath():
+	#called every frame to ensure the translation of the path is the same as the dynamic shape
+	#this is a temp cheat to take advantage of the curve editor of path
 	path.position = Vector2(0,0)
 	path.rotation = 0
 	path.scale = Vector2(1,1)
 
 func _process(delta):
+	#create a path2d child if none exists this is absolutely neceserry and should never be deleted
 	if $Path2D == null:
 		var p = Path2D.new()
 		add_child(p)
 		p.set_owner(get_tree().get_edited_scene_root())
-		#path.curve.connect("changed",self, "update")
+		#everytime the curve changes redraw
+		path.curve.connect("changed",self, "update")
 	else:
+		#ensure path is set
 		path = $Path2D
-		#path.curve.connect("changed",self, "update")
+		if !path.curve.is_connected("changed",self, "update"):
+			path.curve.connect("changed",self, "update")
 	resetpath()
+	
+	#this sets the end point and start point to the same position when the curve is set to closed this still allows moving the points together
 	if closed and  path.curve.get_point_count() > 3 and path.curve.get_point_position(0) != path.curve.get_point_position(path.curve.get_point_count()-1):
 		path.curve.set_point_position(path.curve.get_point_count()-1, path.curve.get_point_position(0))
-	update()
-
-#func setCurve(var c):
-#	if Curve != null:
-#		Curve.disconnect("changed", self, "update")
-#
-#	if c == null:
-#		Curve = Curve2D.new()
-#		Curve.add_point(-Vector2(400,200),-Vector2(700,200),-Vector2(0,200))
-#		Curve.add_point(-Vector2(-400,-200),-Vector2(-0,-200),-Vector2(-700,-200))
-#		print("custom default")
-#	else:
-#		Curve = c
-#
-#	Curve.connect("changed", self, "update")
-#
-#	update()
 
 func drawFill():
 	#dont fill when theres less than 3 points or when closed is not enabled
@@ -120,7 +116,6 @@ func drawBorderPoly(var offset):
 	colors.push_back(Color(1,1,1))
 	
 	draw_polygon(polygon,colors, uvs,texture)
-#	draw_polyline(polygon,Color.white,1)
 	
 	drawBorderPoly(offset+texture.get_width()*factor)
 
@@ -186,8 +181,6 @@ func drawBorderSegmentedPoly():
 			normal2 = baked_points[i+2] - baked_points[i]
 		normal2 = Vector2(-normal2.y,normal2.x).normalized()
 		
-		draw_line(baked_points[i], baked_points[i]-normal*thicknes/2, Color.red,1)
-		
 		var rangeid=style.getAngleRangeID(wrapi(rad2deg(normal.angle())-90,0,359))
 		var texture = style.getTexture(rangeid,0)
 		
@@ -196,12 +189,6 @@ func drawBorderSegmentedPoly():
 		quad.push_back(baked_points[i+1]-normal2*style.getOffset(rangeid)*thicknes)
 		quad.push_back(baked_points[i+1]+normal2*(1-style.getOffset(rangeid))*thicknes)
 		quad.push_back(baked_points[i]+normal*(1-style.getOffset(rangeid))*thicknes)
-		
-#		if closed and i == baked_points.size()-2:
-#			normal = baked_points[1] - baked_points[0]
-#			normal = Vector2(-normal.y,normal.x).normalized()
-#			quad[1] = baked_points[0]-normal/2*thicknes
-#			quad[2] = baked_points[0]+normal/2*thicknes
 		
 		uv_remember_spot = wrapf(uv_remember_spot,0,1)
 		var increase = path.curve.bake_interval/texture.get_width()*(texture.get_height()/thicknes)
@@ -244,17 +231,8 @@ func drawBorderSegmentedPoly():
 		uv.push_back(Vector2(uv_remember_spot+increase,0.01))
 		uv.push_back(Vector2(uv_remember_spot+increase,0.99))
 		uv.push_back(Vector2(uv_remember_spot,0.99))
-#		drawPoint(quad[0],Color.green)
-#		drawPoint(quad[1],Color.red)
-#		drawPoint(quad[2],Color.green)
-#		drawPoint(quad[3],Color.red)
 		
 		quad = fixQuad(quad, colors)
-#
-#		drawPoint(quad[0],Color.green)
-#		drawPoint(quad[1],Color.green)
-#		drawPoint(quad[2],Color.green)
-#		drawPoint(quad[3],Color.green)
 		
 		draw_polygon(quad,colors,uv,texture)
 		
@@ -294,53 +272,65 @@ func drawCorners():
 	for i in range(path.curve.get_point_count()-1):
 		var angle
 		var curve = path.curve
+		#the position of the corner on the curve
 		var point = curve.get_point_position(i)
+		#the 2 vectors going in both directions used to calculate the angle for the corner treshhold
 		var vector1 = (curve.interpolate_baked(curve.get_closest_offset(point)-thicknes)-point).normalized()
 		var vector2 = (curve.interpolate_baked(curve.get_closest_offset(point)+thicknes)-point).normalized()
 		
+		#a fix for the start/end points if its the first and its a closed shape also 
+		#calculate the backwards going vector based on the endlength -thickness as offset
 		if i == 0 and closed:
 			vector1 = (curve.interpolate_baked((curve.get_baked_length()-thicknes))-point).normalized()
 		
-		draw_line(point,point+vector1*10,Color.yellow, 2,true)
-		draw_line(point,point+vector2*10,Color.red, 2,true)
+		#when in editor draw the vectors to visualise the corners and their angles
+		if Engine.is_editor_hint():
+			draw_line(point,point+vector1*10,Color.yellow, 2,true)
+			draw_line(point,point+vector2*10,Color.red, 2,true)
+		
+		#calculate the angle using the 2 vectors and convert to degrees for ease of working with
 		angle = wrapf(rad2deg(vector2.angle_to(vector1)),0,359)
 		
+		#something went wrong if its a closed curve and the angle is 0 at the first point
 		if angle ==0 and closed:
 			if i ==0:
 				print("oops")
-
-		if angle <=120 and angle!=0:
-			drawPoint(point,Color.blue)
+		
+		#determine if its a angle based on angle treshhold
+		if angle <=angle_treshold.y and angle!=0 and angle >= angle_treshold.x:
+			#in editor draw a blue dot signifying it is reconised as a corner used for when a texture is missing
+			#when there is a texture it is obscured
+			if Engine.is_editor_hint():
+				drawPoint(point,Color.blue)
 			
+			#calculate the normal of the corner using the 2 vectors
 			var normal = vector2-vector1
 			normal = Vector2(normal.y,-normal.x).normalized()
 			
+			#draw the normal line 
 			draw_line(point, point+normal*10,Color.red,1)
+			
+			#variables for the texture and the points used to draw the corner texture
 			var points = PoolVector2Array()
 			var tex
-			var offset
 			
-			offset = Vector2(0,0)
-			
+			#empty variable containing future uvs
 			var uvs = PoolVector2Array()
 			
 			#calculate the length towards the corner point and multiply by normal to get coordinate
 			var l = normal*(thicknes/2)/sin(deg2rad((angle/2)))
 			points.push_back(point+l)
 			
-#			var cornerstickout = 60/angle
-#			points.push_back((point+normal*thicknes*cornerstickout))
+			#find what kind of corner it is and set uvs and texture based on that
 			if normal.x > 0:
 				if normal.y > 0:
 					tex = style.rightBottomOuterCorner
-					offset = Vector2(-thicknes, -thicknes)/2
 					uvs.push_back(Vector2(1,1))
 					uvs.push_back(Vector2(1,0))
 					uvs.push_back(Vector2(0,0))
 					uvs.push_back(Vector2(0,1))
 				else:
 					tex = style.rightTopOuterCorner
-					offset = Vector2(-thicknes, thicknes)/2
 					uvs.push_back(Vector2(1,0))
 					uvs.push_back(Vector2(0,0))
 					uvs.push_back(Vector2(0,1))
@@ -348,20 +338,19 @@ func drawCorners():
 			else:
 				if normal.y > 0:
 					tex = style.leftBottomOuterCorner
-					offset = Vector2(thicknes, -thicknes)/2
 					uvs.push_back(Vector2(0,1))
 					uvs.push_back(Vector2(1,1))
 					uvs.push_back(Vector2(1,0))
 					uvs.push_back(Vector2(0,0))
 				else:
 					tex = style.leftTopOuterCorner
-					offset = Vector2(thicknes, thicknes)/2
 					uvs.push_back(Vector2(0,0))
 					uvs.push_back(Vector2(0,1))
 					uvs.push_back(Vector2(1,1))
 					uvs.push_back(Vector2(1,0))
 			
-			
+			#calculate corner point depending on normal of earlier or further point 
+			#this is done in a way that closed end points still work
 			var p = point+vector1*thicknes/2
 			p = curve.interpolate_baked(curve.get_closest_offset(p))
 			var p_normal = curve.interpolate_baked(curve.get_closest_offset(p)+thicknes/4)-curve.interpolate_baked(curve.get_closest_offset(p)-thicknes/4)
@@ -380,18 +369,16 @@ func drawCorners():
 			drawPoint(p, Color.red)
 			points.push_back(p)
 			
+			#just assign white for the texture
 			var colors = PoolColorArray()
 			colors.push_back(Color.white)
 			colors.push_back(Color.white)
 			colors.push_back(Color.white)
 			colors.push_back(Color.white)
 			
-			draw_multiline(points, Color.white,2, true)
-			draw_polygon(points,colors,uvs,tex)
-#			points.push_back(Vector2(point.x-normal.x*thicknes/2,point.y+normal.y*thicknes/2)+offset)
-			
-#			draw_texture_rect(tex, Rect2(point.x+normal.x*0.5*thicknes, point.y+normal.y*0.5*thicknes,thicknes,thicknes), false)
-			
+			#only draw when theres a texture
+			if tex != null:
+				draw_polygon(points,colors,uvs,tex)
 
 func drawBorder():
 	if !segmented:
@@ -403,42 +390,6 @@ func drawBorder():
 	
 	if drawcorners:
 		drawCorners()
-#	drawBorderSegmentedMesh()
-	#loop over points
-	
-#	for i in range(path.curve.get_baked_points()-1):
-#		var baked_points = path.curve.get_baked_points()
-#
-#		var normal = baked_points[i+1] - baked_points[i]
-#		normal = Vector2(normal.y,-normal.x)
-#
-#
-	
-#	var previous_p = null
-#	var i =0
-#	for p in path.curve.get_baked_points():
-#		#get normal of point
-#		if previous_p != null:
-#			var normal = (p-previous_p)
-#			normal = Vector2(normal.y,-normal.x)
-#			var middle = previous_p+((p-previous_p)/2)
-#
-#			var rangeid=style.getAngleRangeID(rad2deg(normal.angle()))
-##			draw_set_transform(middle-normal*-5,normal.angle()+deg2rad(90), Vector2(.2,.2))
-##			draw_texture(style.getTexture(rangeid,0),Vector2(0,0))
-##			draw_set_transform(Vector2(0,0),0, Vector2(1,1))
-#
-#			#draw normal only every 10th and half a normal every 5th
-##			if i==5:
-##				drawNormal(middle, normal/2)
-##			if i==10:
-##				drawNormal(middle, normal)
-##				i=0
-##			i+=1
-		
-#		previous_p = p
-		#determine what texture to draw
-		#draw the texture
 
 func drawBorderSegmentedCurve():
 	pass
